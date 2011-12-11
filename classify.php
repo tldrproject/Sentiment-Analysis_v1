775252cf959429a6adcab5098fb11443baa37203
poindexterc <poindexterc@gmail.com>
2011-12-10 20:58:26 -0500

    Update classify.php

--- classify.php
+++ classify.php
@@ -20,31 +20,52 @@
   $sentimentData->addToIndex('positivedata.txt', 'positive');
   
   //Add your data in a "$doc" variable.
+  
+  //Put sentances of string into array.
   $sentences = explode(".", $doc);
+  
+  //Create array for positive and negative sentiment
   $score = array('positive' => 0, 'negative' => 0);
+  
+  //Loop through sentances and find sentiment
   foreach($sentences as $sentence) {
           if(strlen(trim($sentence))) {
                   $class = $sentimentData->classify($sentence);
+                 
+                 //Add sentiment to sentiment score
                   $score[$class]++;
           }
   }
   
   //<do better> 
   //This isn't the cleanest it could be. If you can think of a better way of doing this, please tell me. cpopensource [at] gmail.com
+  
+  //Reverse sort the score in order to find the most likely sentiment (positive or negative)
   arsort($score);
+  
+  //To find how much bias there is (assurance), divide the positive sentiment score by the negative
   $assurance = $score['positive']/$score['negative'];
+  
+  //If the assurance is not a decimal...
   if($assurance>1){
       $assurance = $score['negative']/$score['positive'];
-  } 
+  }
+  
+  //Remove the least likey alternative 
   array_pop($score);
   
   //<do better/>
   
+  //As the sentiment (positive and negative) are the keys in the array, find the key of the sentiment.
   $keyArray = array_keys($score);
-  if($assurance>.4){
+  
+  //If we're not sure (the assurance (above) is less than .47, its inconclusive. Otherwise, it's most likely good.
+  if($assurance>.47){
       $sentiment = $keyArray[0];
   } else {
       $sentiment = "inconclusive";
   }
+  
+  //Echo out the sentiment
   echo $sentiment;
 ?>
