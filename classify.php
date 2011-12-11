391ac1bf1c7d06be91369662c35cbe64062e91c2
Colin Poindexter <poindexterc@gmail.com>
2011-12-10 21:56:13 -0500

    Update classify.php

--- classify.php
+++ classify.php
@@ -1,18 +1,24 @@
 <?php
-  //Sentiment Analysis (Classification) using bayesian Opinion Mining
-  //Copyright 2011 The tldr Project c/o ipsumedia Limited. 
 
-  //Licensed under the Apache License, Version 2.0 (the "License");
-  //you may not use this file except in compliance with the License.
-  //You may obtain a copy of the License at
 
-      //http://www.apache.org/licenses/LICENSE-2.0
+/* 
+  Sentiment Analysis (Classification) using bayesian Opinion Mining
+  Copyright 2011 The tldr Project c/o ipsumedia Limited. 
+  
+  
+  LICENSE: 
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
 
-  //Unless required by applicable law or agreed to in writing, software
-  //distributed under the License is distributed on an "AS IS" BASIS,
-  //WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-  //See the License for the specific language governing permissions and
-  //limitations under the License.
+       http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+*/
   
   include('Opinion.class.php');
   $sentimentData = new Opinion();
